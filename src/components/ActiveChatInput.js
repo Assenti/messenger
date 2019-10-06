9c41ca028f28208218453c98b9c16c29425fb32a
Asset Sultanov <asset.sultan@gmail.com>
2019-10-06 21:12:18 +0600

    Working with event handling and two-way data binding

--- src/components/ActiveChatInput.js
+++ src/components/ActiveChatInput.js
@@ -1,18 +1,44 @@
 import React, { Component } from 'react'
 
 export class ActiveChatInput extends Component {
-  state = {
-      
-  }
+    constructor(props) {
+        super(props)
 
-  render() {
-    return (
-      <div className="active-chat__input">
-          <input />
-          <button className="btn secondary">send</button>
-      </div>
-    )
-  }
+        this.state = {
+            message: ''
+        }
+
+        this.handleChange = this.handleChange.bind(this)
+        this.clearMessage = this.clearMessage.bind(this)
+        this.sendMessage = this.sendMessage.bind(this)
+    }
+
+    handleChange(e) {
+        this.setState({message: e.target.value})
+    }
+
+    sendMessage(e) {
+        console.log(this.state.message)
+        e.preventDefault()
+        this.clearMessage()
+    }
+
+    clearMessage() {
+        this.setState({ message: '' })
+    }
+
+    render() {
+        return (
+        <form className="active-chat__input" 
+            onSubmit={this.sendMessage}>
+            <input value={this.state.message}
+                onChange={this.handleChange}/>
+            <button className="btn secondary"
+                type="submit" 
+                onClick={this.sendMessage}>send</button>
+        </form>
+        )
+    }
 }
 
 export default ActiveChatInput
